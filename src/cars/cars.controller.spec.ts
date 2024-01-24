6c0d33f30f4f3e5c806f45c7640a6302a7b1b1a3
Christian Santos <christiansantos.dev@gmail.com>
2024-01-24 06:50:07 -0300

    test: add test cases to car service and controller

--- src/cars/cars.controller.spec.ts
+++ src/cars/cars.controller.spec.ts
@@ -1,3 +1,4 @@
+import { PrismaModule } from './../prisma/prisma.module';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CarsController } from './cars.controller';
 import { CarsService } from './cars.service';
@@ -7,6 +8,7 @@ describe('CarsController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       controllers: [CarsController],
       providers: [CarsService],
     }).compile();
@@ -16,5 +18,91 @@ describe('CarsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return all cars', () => {
+    const cars = [
+      {
+        id: 1,
+        brand: 'Brand 1',
+        model: 'Model 1',
+        package: 'Package 1',
+        color: 'Color 1',
+        year: 2020,
+        category: 'Category 1',
+        mileage: 2000,
+        price: 20000,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 2,
+        brand: 'Brand 2',
+        model: 'Model 2',
+        package: 'Package 2',
+        color: 'Color 2',
+        year: 2020,
+        category: 'Category 2',
+        mileage: 2000,
+        price: 20000,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+    jest.spyOn(controller, 'findAll').mockResolvedValue(cars);
+    expect(controller.findAll()).resolves.toEqual(cars);
+  });
+
+  it('should create a new car with all optional properties filled in', () => {
+    const createCarDto = {
+      brand: 'Ferrari',
+      model: 'Ferrari 458',
+      package: 'Italia',
+      color: 'Red',
+      year: 2015,
+      category: 'Sport',
+      mileage: 10000,
+      price: 1000000,
+    };
+    const createdCar = {
+      id: 1,
+      ...createCarDto,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const createMock = jest.fn().mockResolvedValue(createdCar);
+    jest
+      .spyOn(controller, 'create')
+      .mockImplementation(() => createMock(createCarDto));
+
+    expect(controller.create(createCarDto)).resolves.toEqual(createdCar);
+  });
+
+  it('should return null when trying to find a car with an id that does not exist', () => {
+    const id = '999';
+    jest.spyOn(controller, 'findOne').mockReturnValue(null);
+    expect(controller.findOne(id)).toBeNull();
+  });
+  it('should update a car', async () => {
+    const updateCarDto = {
+      brand: 'Updated Brand',
+      model: 'Updated Model',
+      package: 'Updated Package',
+      color: 'Updated Color',
+      year: 2021,
+      category: 'Updated Category',
+      mileage: 3000,
+      price: 25000,
+    };
+    const updatedCar = {
+      id: 1,
+      ...updateCarDto,
+      createdAt: new Date('2020-01-01'),
+      updatedAt: new Date(),
+    };
+    jest.spyOn(controller, 'update').mockResolvedValue(updatedCar);
+    await expect(controller.update('1', updateCarDto)).resolves.toEqual(
+      updatedCar,
+    );
   });
 });
--- src/cars/cars.service.spec.ts
+++ src/cars/cars.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CarsService } from './cars.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 describe('CarsService', () => {
   let service: CarsService;
 
+  const mockCarsService = {
+    findAll: jest.fn(() => []),
+    findOne: jest.fn((id) => ({ id, model: 'Test Car', year: 2022 })),
+    create: jest.fn((carDto) => ({ id: Date.now(), ...carDto })),
+    update: jest.fn((id, carDto) => ({ id, ...carDto })),
+    remove: jest.fn((id) => ({ id })),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CarsService],
+      imports: [PrismaModule], // Add this line to import PrismaModule
+      providers: [
+        {
+          provide: CarsService,
+          useValue: mockCarsService,
+        },
+      ],
     }).compile();
 
     service = module.get<CarsService>(CarsService);
@@ -14,5 +29,54 @@ describe('CarsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should find all cars', async () => {
+    expect(await service.findAll()).toEqual([]);
+    expect(mockCarsService.findAll).toHaveBeenCalled();
+  });
+
+  it('should find one car by id', async () => {
+    const carId = 1;
+    expect(await service.findOne(carId)).toEqual({
+      id: carId,
+      model: 'Test Car',
+      year: 2022,
+    });
+    expect(mockCarsService.findOne).toHaveBeenCalledWith(carId);
+  });
+
+  it('should create a car', async () => {
+    const newCar = {
+      brand: 'Ferrari',
+      model: 'Ferrari 458',
+      package: 'Italia',
+      color: 'Red',
+      year: 2015,
+      category: 'Sport',
+      mileage: 10000,
+      price: 1000000,
+    };
+    const createdCar = await service.create(newCar);
+    expect(createdCar).toHaveProperty('id');
+    expect(createdCar.model).toEqual(newCar.model);
+    expect(createdCar.year).toEqual(newCar.year);
+    expect(mockCarsService.create).toHaveBeenCalledWith(newCar);
+  });
+
+  it('should update a car', async () => {
+    const carId = 1;
+    const carUpdateDto = { model: 'Updated Car', year: 2023 };
+    expect(await service.update(carId, carUpdateDto)).toEqual({
+      id: carId,
+      ...carUpdateDto,
+    });
+    expect(mockCarsService.update).toHaveBeenCalledWith(carId, carUpdateDto);
+  });
+
+  it('should remove a car', async () => {
+    const carId = 1;
+    expect(await service.remove(carId)).toEqual({ id: carId });
+    expect(mockCarsService.remove).toHaveBeenCalledWith(carId);
   });
 });

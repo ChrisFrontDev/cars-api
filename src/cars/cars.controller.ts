9caceb34004ecdfaac1c70c919caf582e453c6d0
Christian Santos <christiansantos.dev@gmail.com>
2024-01-24 05:53:18 -0300

    chore: Add Swagger annotations to CarsController and CreateCarDto

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -6,21 +6,32 @@ import {
   Patch,
   Param,
   Delete,
+  HttpStatus,
 } from '@nestjs/common';
+import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
+
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
 import { UpdateCarDto } from './dto/update-car.dto';
 
+@ApiTags('cars')
 @Controller('cars')
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
+  @ApiOperation({ summary: 'Add a new car' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The car has been successfully created.',
+  })
   @Post()
   create(@Body() createCarDto: CreateCarDto) {
     return this.carsService.create(createCarDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all cars' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Return all cars.' })
   findAll() {
     return this.carsService.findAll();
   }
--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,10 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCarDto {
+  @ApiProperty({ example: 'Toyota' })
   brand: string;
+
+  @ApiProperty({ example: 'Corolla' })
   model: string;
+
+  @ApiProperty({ example: 'XSE' })
   package: string;
+
+  @ApiProperty({ example: 'red' })
   color: string;
+
+  @ApiProperty({ example: 2021 })
   year: number;
+
+  @ApiProperty({ example: 'sedan' })
   category: string;
+
+  @ApiProperty({ example: 100 })
   mileage: number;
+
+  @ApiProperty({ example: 20000 })
   price: number;
 }
